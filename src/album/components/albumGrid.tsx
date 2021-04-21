6fb2d8b52a2f956e3a1172bdce58fb5704185028
Wojciech Malarski <w.malarski@gmail.com>
2021-04-21 20:44:44 +0200

    skeletons imporoved

--- src/album/components/albumGrid.tsx
+++ src/album/components/albumGrid.tsx
@@ -26,8 +26,8 @@ export default function AlbumGrid(props: AlbumGridProps): JSX.Element {
       <SimpleGrid minChildWidth="230px" spacing="10px">
         {isLoading
           ? range(0, defaultCount).map(index => (
-              <Card key={index} height="230px">
-                <Skeleton height="190px" />
+              <Card key={index} height="250px">
+                <Skeleton height="100%" width="100%" />
               </Card>
             ))
           : albums?.map(album => (
--- src/common/functions.ts
+++ src/common/functions.ts
@@ -3,5 +3,7 @@ export function compact<T>(arr?: (T | null | undefined)[] | null): T[] {
 }
 
 export function range(from: number, to: number): number[] {
-  return Array(Math.min(to - from, 0)).map((_val, index) => index + from);
+  return Array(Math.max(to - from, 0))
+    .fill(1)
+    .map((_val, index) => index + from);
 }
